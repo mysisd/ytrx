ca08d857f95c50f06549fad249ad556c14864ca9
陈育智 <2357638361@qq.com>
2018-03-23 15:12:16 +0800

    修改

--- sz/manager/controller/User.php
+++ sz/manager/controller/User.php
@@ -8,6 +8,7 @@ namespace app\manager\controller;
 use app\manager\controller\Base;
 use app\manager\model\User as XgjuserModel;
 use think\Csv;
+use think\PHPExcel;
 class User extends Base {
 	public function yt_xgjuser() {
 		if(input('delete')!='') {
@@ -30,36 +31,46 @@ class User extends Base {
 			return $this->fetch();
 		}
 	}
-	public function export() {
+	public function export(){
+        if(!empty($_POST['code'])) {
+            $_SESSION['csv_code'] = 123456;
+            if(!empty($_SESSION['csv_code']) && $_POST['code'] == $_SESSION['csv_code']){
+                $list = XgjuserModel::select();
+                $csv_title=array('id','期货账户','姓名','绑定银行卡','银行名称','开户省','开户市','支行名称','支付宝账号','手机号','代理代码','注册时间');
+                $this->createtable($list,'盈透锐新-用户信息表',$csv_title,$csv_title);
 
-
-				$list = XgjuserModel::select();
-				$csv_title=array('id','手机号','姓名','QQ','微信','银行卡','喜好品种','邀请码','注册时间');
-               			Csv::put_csv($list,$csv_title);
-
-
-	}
-   public function  export_csv($data)
-    {
-        $string="";
-        foreach ($data as $key => $value)
-        {
-            foreach ($value as $k => $val)
-            {
-                $value[$k]=iconv('utf-8','gb2312',$value[$k]);
             }
-
-            $string .= implode(",",$value)."\n"; //用英文逗号分开
         }
-        $filename = date('Ymd').'.csv'; //设置文件名
-        header("Content-type:text/csv");
-        header("Content-Disposition:attachment;filename=".$filename);
-        header('Cache-Control:must-revalidate,post-check=0,pre-check=0');
-        header('Expires:0');
-        header('Pragma:public');
-        echo $string;
+
+
     }
-	public function detail_xgjuser()
+    /**
+     * 创建(导出)Excel数据表格
+     * @param  array   $list 要导出的数组格式的数据
+     * @param  string  $filename 导出的Excel表格数据表的文件名
+     * @param  array   $header Excel表格的表头
+     * @param  array   $index $list数组中与Excel表格表头$header中每个项目对应的字段的名字(key值)
+     * 比如: $header = array('编号','姓名','性别','年龄');
+     *       $index = array('id','username','sex','age');
+     *       $list = array(array('id'=>1,'username'=>'YQJ','sex'=>'男','age'=>24));
+     * @return [array] [数组]
+     */
+    protected function createtable($list,$filename,$header=array(),$index = array()){
+        header("Content-type:application/vnd.ms-excel");
+        header("Content-Disposition:filename=".$filename.".xls");
+        $teble_header = implode("\t",$header);
+        $strexport = $teble_header."\r";
+        foreach ($list as $row){
+            foreach($index as $val){
+                $strexport.=$row[$val]."\t";
+            }
+            $strexport.="\r";
+
+        }
+        $strexport=iconv('UTF-8',"GB2312//IGNORE",$strexport);
+        exit($strexport);
+    }
+    public function detail_xgjuser()
     {
         if (input('id')!='') {
             $userinfo = Db('user')->where('id', input('id'))->find();
